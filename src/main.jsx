67f02095de54426c0ccb750016c691398eb8ffea
SunainaNelli <sunaina.nelli@gmail.com>
2025-08-08 11:29:52 +0530

    Improving the routinf experience

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import {store} from './app/store.js';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -14,7 +14,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <Provider store={store}>
-       <BrowserRouter>
+       <HashRouter>
          <App />
          <ToastContainer
            position="bottom-right"
@@ -26,7 +26,7 @@ createRoot(document.getElementById('root')).render(
            transition={Bounce} // or slide, zoom, flip
           
          />
-       </BrowserRouter>
+       </HashRouter>
      </Provider>
   </StrictMode>,
 )
